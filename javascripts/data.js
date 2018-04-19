23b9f33139faa21dced0e7623c76ae1ff4aff555
Amanda Mitchell <wreedamitchell@gmail.com>
2018-04-18 20:30:35 -0500

    get and set created for cat info

---
+++ javascripts/data.js
@@ -0,0 +1,25 @@
+// let movies = [];
+let categories = [];
+
+// const getMovies = () => {
+//   return movies;
+// };
+
+// const setMovies = (moviesArray) => {
+//   movies = moviesArray;
+// };
+
+const getCats = () => {
+  return categories;
+};
+
+const setCats = (categoriesArray) => {
+  categories = categoriesArray;
+};
+
+module.exports = {
+  getCats,
+  setCats,
+  // getMovies,
+  // setMovies,
+};
