8aeeaa2613317fd5dac11748aad5703e1c8fc36a
Amanda Mitchell <wreedamitchell@gmail.com>
2018-04-18 20:29:27 -0500

    created catonload function

--- javascripts/dataGateKeeper.js
+++ javascripts/dataGateKeeper.js
@@ -1,21 +1,28 @@
-const movieLoad = require('./movie');
+// const movieLoad = require('./movie');
+// const movieDom = require('./movieDom');
+const categoryLoad = require('./categories');
+const printCategoriesToDom = require('./categoryDom');
+const data = require('./data');
 
-// const catOnLoad = function () {
-//   // const categoryData = JSON.parse(this.responseText).categories;
-//   // data.setCategories(categoryData);
-// };
-
-const onLoad = function () {
-  // const movieData = JSON.parse(this.responseText).movie;
-  // data.setMovies(movieData);
+const catOnLoad = function () {
+  const categoryData = JSON.parse(this.responseText).categories;
+  data.setCats(categoryData);
+  printCategoriesToDom(categoryData);
 };
+
+// const onLoad = function () {
+//   const movieData = JSON.parse(this.responseText).movie;
+//   data.setMovies(movieData);
+//   movieDom(movieData);
+// };
 
 const noLoad = function () {
   console.log('Cannot Load');
 };
 
 const success = () => {
-  movieLoad(onLoad, noLoad);
+  // movieLoad(catOnLoad, noLoad);
+  categoryLoad(catOnLoad, noLoad);
 };
 
 module.exports = success;
